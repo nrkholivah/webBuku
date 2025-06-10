068850adf04a29d641442de1448ac3f543876a98
PEMWEB1 <nurkholivah1701@gmail.com>
2025-06-10 23:02:51 +0700

    upload file in ci4

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,6 +11,7 @@ $routes->get('/tentang', 'Books::tentang');
 $routes->get('/hubungi', 'Books::hubungi');
 
 $routes->get('/', 'Books::index');
+$routes->get('/books', 'Books::index');
 $routes->get('/books/tambah', 'Books::tambah');
 $routes->post('/books', 'Books::save');
 $routes->get('/books/edit/(:segment)', 'Books::edit/$1');
@@ -18,4 +19,3 @@ $routes->post('/books/update/(:num)', 'Books::update/$1');
 $routes->get('/books/(:segment)', 'Books::delete/$1');
 $routes->delete('/books/(:num)', 'Books::delete/$1');
 $routes->get('/books/(:segment)', 'Books::detail/$1');
-
